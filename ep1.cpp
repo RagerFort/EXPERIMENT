9a80de3eeb72633d78d4b4d0b3a1d4e9eac6db85
RagerFort <ragerfort@gmail.com>
2025-02-10 00:24:40 +0000

    created executable

--- ep1.cpp
+++ ep1.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 using namespace std;
 int main (){
-	cout << "Hello World!"; 
-	system ("pause");
+	int num1,num2,sum;
+	cout << "Enter first number: ";
+	cin >> num1;
+	cout << "Enter second number: ";
+	cin >> num2;
+	sum = num1 + num2;
+	cout << "The sum of " <<sum << endl;
+	return 0;
 }
