f39791759c890dfa36db0047135b5bc675780815
RagerFort <ragerfort@gmail.com>
2025-02-16 08:02:35 +0000

    refactor: improve formatting and structure in multiple C++ programs

--- labact5.cpp
+++ labact5.cpp
@@ -1,19 +1,24 @@
 #include <iostream>
 #include <stdlib.h>
 using namespace std;
+
 char x[5];
 char y;
 int z;
 
-main()
-{ 
-system ("cls");
-cout<<"Enter a name";
-cin>>x;
-cout<<"You entered "<<x<<endl;
-cout<<endl<<"Enter letter to search: ";
-cin>>y;
-for(z=0;z<=4;z++)
-if (y==x[z])
-  cout<<y<<"is located at "<<"array["<<z<<"]"<<endl;
+int main() { 
+    system("clear");
+    cout << "Enter a name: ";
+    cin >> x;
+    cout << "You entered " << x << endl;
+    cout << endl << "Enter letter to search: ";
+    cin >> y;
+    
+    for (z = 0; z <= 4; z++) {
+        if (y == x[z]) {
+            cout << y << " is located at array[" << z << "]" << endl;
+        }
+    }
+    
+    return 0;
 }
