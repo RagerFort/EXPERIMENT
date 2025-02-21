6b6a96971867898b01dc200ecad646f29a3045e6
RagerFort <ragerfort@gmail.com>
2025-02-21 15:13:51 +0000

    feat: replace ASCII art generation with retail price calculation functionality

--- acii_art.cpp
+++ acii_art.cpp
@@ -1,33 +1,19 @@
 #include <iostream>
 using namespace std;
+double wholesaleCost, markupPercentage;
+double calculateRetail( wholesaleCost, markupPercentage) {
+    return wholesaleCost + (wholesaleCost * markupPercentage / 100);
+}
+
 int main() {
-    const int n = 5; 
-
-    // Upper part of the star
-    for (int i = 0; i < n; i++) {
-        // Print spaces
-        for (int j = 0; j < n - i - 1; j++) {
-            cout << " ";
-        }
-        // Print stars
-        for (int j = 0; j < 2 * i + 1; j++) {
-            cout << "*";
-        }
-        cout << endl; // move to next line
-    }
-
-    // Lower part of the diamond
-    for (int i = n - 2; i >= 0; i--) { // start from second last row
-        // Print spaces
-        for (int j = 0; j < n - i - 1; j++) {
-            std::cout << " ";
-        }
-        // Print stars
-        for (int j = 0; j < 2 * i + 1; j++) {
-            std::cout << "*";
-        }
-        std::cout << std::endl; // move to next line
-    }
+    
+    cout << "Enter the item's wholesale cost: ";
+    cin >> wholesaleCost;
+    cout << "Enter the item's markup percentage: ";
+    cin >> markupPercentage;
+wholesaleCost = wholesaleCost+(wholesaleCost*markupPercentage/100);
+    double retailPrice = calculateRetail(wholesaleCost, markupPercentage);
+    cout << "The item's retail price is: " << retailPrice << endl;
 
     return 0;
 }
