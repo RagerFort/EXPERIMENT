65bd581783684ba91137491ef707745be6c72227
RagerFort <ragerfort@gmail.com>
2025-03-11 11:58:32 +0000

    refactor: remove std:: prefix for improved code readability in queue implementation

--- MP 4/2.cpp
+++ MP 4/2.cpp
@@ -1,68 +1,70 @@
 #include <iostream>
 #include <queue>
 
+using namespace std;
+
 void displayMenu() {
-    std::cout << "[A] Push Data\n";
-    std::cout << "[B] Pop Data\n";
-    std::cout << "[C] Show Front\n";
-    std::cout << "[D] Show Back\n";
-    std::cout << "[E] Show Size\n";
-    std::cout << "[F] Quit\n";
-    std::cout << "Enter Choice: ";
+    cout << "[A] Push Data\n";
+    cout << "[B] Pop Data\n";
+    cout << "[C] Show Front\n";
+    cout << "[D] Show Back\n";
+    cout << "[E] Show Size\n";
+    cout << "[F] Quit\n";
+    cout << "Enter Choice: ";
 }
 
 int main() {
-    std::queue<int> q;
+    queue<int> q;
     char choice;
     int value;
 
     do {
         displayMenu();
-        std::cin >> choice;
+        cin >> choice;
 
         switch (choice) {
             case 'A':
             case 'a':
-                std::cout << "Enter value to push: ";
-                std::cin >> value;
+                cout << "Enter value to push: ";
+                cin >> value;
                 q.push(value);
-                std::cout << "Value pushed.\n";
+                cout << "Value pushed.\n";
                 break;
             case 'B':
             case 'b':
                 if (!q.empty()) {
                     q.pop();
-                    std::cout << "Value popped.\n";
+                    cout << "Value popped.\n";
                 } else {
-                    std::cout << "Queue is empty.\n";
+                    cout << "Queue is empty.\n";
                 }
                 break;
             case 'C':
             case 'c':
                 if (!q.empty()) {
-                    std::cout << "Front value: " << q.front() << "\n";
+                    cout << "Front value: " << q.front() << "\n";
                 } else {
-                    std::cout << "Queue is empty.\n";
+                    cout << "Queue is empty.\n";
                 }
                 break;
             case 'D':
             case 'd':
                 if (!q.empty()) {
-                    std::cout << "Back value: " << q.back() << "\n";
+                    cout << "Back value: " << q.back() << "\n";
                 } else {
-                    std::cout << "Queue is empty.\n";
+                    cout << "Queue is empty.\n";
                 }
                 break;
             case 'E':
             case 'e':
-                std::cout << "Queue size: " << q.size() << "\n";
+                cout << "Queue size: " << q.size() << "\n";
                 break;
             case 'F':
             case 'f':
-                std::cout << "Quitting...\n";
+                cout << "Quitting...\n";
                 break;
             default:
-                std::cout << "Invalid choice. Please try again.\n";
+                cout << "Invalid choice. Please try again.\n";
         }
     } while (choice != 'F' && choice != 'f');
 
