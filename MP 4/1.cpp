fc62864912efe61cd862de74e4fb52f67ac12fe2
RagerFort <ragerfort@gmail.com>
2025-03-11 11:56:24 +0000

    refactor: simplify code by removing std:: prefix for standard library usage

--- MP 4/1.cpp
+++ MP 4/1.cpp
@@ -1,59 +1,61 @@
 #include <iostream>
 #include <stack>
 
+using namespace std;
+
 void displayMenu() {
-    std::cout << "[A] Push Data\n";
-    std::cout << "[B] Pop Data\n";
-    std::cout << "[C] Show Top\n";
-    std::cout << "[D] Show Size\n";
-    std::cout << "[E] Quit\n";
-    std::cout << "Enter Choice: ";
+    cout << "[A] Push Data\n";
+    cout << "[B] Pop Data\n";
+    cout << "[C] Show Top\n";
+    cout << "[D] Show Size\n";
+    cout << "[E] Quit\n";
+    cout << "Enter Choice: ";
 }
 
 int main() {
-    std::stack<int> s;
+    stack<int> s;
     char choice;
     int value;
 
     do {
         displayMenu();
-        std::cin >> choice;
+        cin >> choice;
 
         switch (choice) {
             case 'A':
             case 'a':
-                std::cout << "Enter value to push: ";
-                std::cin >> value;
+                cout << "Enter value to push: ";
+                cin >> value;
                 s.push(value);
-                std::cout << "Value pushed.\n";
+                cout << "Value pushed.\n";
                 break;
             case 'B':
             case 'b':
                 if (!s.empty()) {
                     s.pop();
-                    std::cout << "Value popped.\n";
+                    cout << "Value popped.\n";
                 } else {
-                    std::cout << "Stack is empty.\n";
+                    cout << "Stack is empty.\n";
                 }
                 break;
             case 'C':
             case 'c':
                 if (!s.empty()) {
-                    std::cout << "Top value: " << s.top() << "\n";
+                    cout << "Top value: " << s.top() << "\n";
                 } else {
-                    std::cout << "Stack is empty.\n";
+                    cout << "Stack is empty.\n";
                 }
                 break;
             case 'D':
             case 'd':
-                std::cout << "Stack size: " << s.size() << "\n";
+                cout << "Stack size: " << s.size() << "\n";
                 break;
             case 'E':
             case 'e':
-                std::cout << "Quitting...\n";
+                cout << "Quitting...\n";
                 break;
             default:
-                std::cout << "Invalid choice. Please try again.\n";
+                cout << "Invalid choice. Please try again.\n";
         }
     } while (choice != 'E' && choice != 'e');
 
